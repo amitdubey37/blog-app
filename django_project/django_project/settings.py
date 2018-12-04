cd4ef820a5a85f033fed6e3ca3949707f7727d34
Amit <amit.dubey@tothenew.com>
2018-12-04 16:14:04 +0530

    test

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -135,5 +135,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'dubeyamitdubey@gmail.com'
-EMAIL_HOST_PASSWORD = 'nftzqeezibymgevv'
+EMAIL_HOST_PASSWORD = 'aanftzqeezibymgevvvb'
 
